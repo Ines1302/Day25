85f39f09dcf7bdc6d2a3ddc790f98927d141b4e1
Ines1302 <seeking.knowledge13@gmail.com>
2021-04-01 23:59:11 +0200

    Create

---
+++ ex02/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GetRequestAsyncAwaint from './GetRequestAsyncAwait';
+
+function App() {
+  return (
+    <div className="App">
+      <GetRequestAsyncAwaint />
+    </div>
+  );
+}
+
+export default App;
---
+++ ex06/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PostRequestAsyncAwaint from './PostRequestAsyncAwait';
+
+function App() {
+  return (
+    <div className="App">
+      <PostRequestAsyncAwaint />
+    </div>
+  );
+}
+
+export default App;
